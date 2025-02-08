3c253f2fc98a46cf60f5257ed3292e5e8a59d0cb
Mert Parlak <mertparlak123@hotmail.com>
2025-02-08 21:48:55 +0300

    adding sing out action on server and client

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,9 +1,15 @@
 "use server";
 
-import { signIn } from "./auth";
+import { signIn, signOut } from "./auth";
 
 export async function signInAction(formData) {
   const provider = formData.get("provider");
 
   await signIn(provider, { redirectTo: "/account" });
+}
+
+export async function singOutAction() {
+  await signOut({
+    redirectTo: "/",
+  });
 }
