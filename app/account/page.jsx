b43b49c2e4f99aa4031b5fc8ec6ddcc717ca3d5e
Mert Parlak <mertparlak123@hotmail.com>
2025-02-08 21:01:04 +0300

    adding current customer name for dynamic page

--- app/account/page.jsx
+++ app/account/page.jsx
@@ -1,9 +1,15 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Account",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+
   return (
-    <h2 className="font-semibold text-2xl text-accent-400 mb-7"> SA MERT</h2>
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome {session?.user?.name.split(" ").at(0)}
+    </h2>
   );
 }
